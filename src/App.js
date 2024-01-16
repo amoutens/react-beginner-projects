5ea8418086326b0a7e9023d776496300d7a1300b
Kate <1gayv425@gmail.com>
2024-01-16 14:49:58 +0200

    fnPlus functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './index.scss';
-
+import React from 'react';
 function App() {
+  const[count, setCount] = React.useState(0);
+  const fnPlus = () => {setCount(count+1)}
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button onClick={fnPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
