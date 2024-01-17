8e28be22deec67a47c089bc3fe9a259bd8567b65
Kate <1gayv425@gmail.com>
2024-01-17 15:16:07 +0200

    add quiz functional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.scss';
-
+import React from 'react';
 const questions = [
   {
     title: 'React - это ... ?',
@@ -22,36 +22,48 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({correctAnswers}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>{`Вы отгадали ${correctAnswers} ответа из ${questions.length}`}</h2>
+      <a href='/'><button>Попробовать снова</button></a>
     </div>
   );
 }
 
-function Game() {
+function Game({step, question, onClickVar}) {
+  const percent = Math.round(step/questions.length*100);
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percent}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((obj, index) => <li onClick={() => onClickVar(index)} key={obj}>{obj}</li>)}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correctAnswers, setAnswer] = React.useState(0);
+  const question = questions[step];
+  const onClickVar  = (index) => {
+    console.log(step, index)
+    if(index === question.correct) setAnswer(correctAnswers+1);
+    setStep(step+1)
+  }
+
   return (
     <div className="App">
-      <Game />
+      {//<Game step={step} question={question} onClickVar={onClickVar} />
+      step !== questions.length? <Game step={step} question={question} onClickVar={onClickVar} /> :  <Result correctAnswers={correctAnswers} /> 
+      
+      
+      }
       {/* <Result /> */}
     </div>
   );
