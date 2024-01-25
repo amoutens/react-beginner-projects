bf513fb71d115d06eb763d22463302ff73e795cf
Kate <1gayv425@gmail.com>
2024-01-25 12:18:42 +0200

    add api fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './index.scss';
-
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+import { Collection } from "./Collection.jsx";
+import { json } from 'body-parser';
 
 function App() {
+  const [collection, setCollection] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("https://65b230d99bfb12f6eafd1c8e.mockapi.io/collection").then(res => res.json).then(json => json)
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
